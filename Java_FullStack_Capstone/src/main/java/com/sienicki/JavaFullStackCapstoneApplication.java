21244607b1d3bbf198472119dcf417a198a98646
Justin Sienicki <justinsienicki@att.net>
2021-11-29 18:40:01 -0600

    first commit

---
+++ Java_FullStack_Capstone/src/main/java/com/sienicki/JavaFullStackCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.sienicki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFullStackCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFullStackCapstoneApplication.class, args);
+	}
+
+}
---
+++ Java_FullStack_Capstone/src/test/java/com/sienicki/JavaFullStackCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sienicki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFullStackCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
